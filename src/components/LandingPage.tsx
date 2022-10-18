e75e6a9842044f7cb319373190cc62bb673c226d
Zion Miller <djzionmiller@gmail.com>
2022-10-18 14:02:25 -0700

    trying absolute positioning for footer

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -66,7 +66,7 @@ const LandingPage = () => {
     <div style=
         {{
             // fontSize: "small",
-            position: "fixed",
+            position: "absolute",
             left: 0,
             bottom: 0,
             right: 0,
